d301882cb7f6d98e067f7d5e57f7704955f64a3b
OkforOmarkhalid <omar.khalid.ismael.pro@gmail.com>
2024-07-01 21:04:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,32 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-let emptySlots = 6;
-const id = 1;  // Static ID
+let buildings = {
+  1: { id: 1, slots: 0 },
+  2: { id: 2, slots: "SOON" },
+  3: { id: 3, slots: "SOON" },
+  4: { id: 4, slots: "SOON" },
+  5: { id: 5, slots: "SOON" }
+};
 
+// Endpoint to update the number of empty slots for Building 1
 app.post('/update-slots', (req, res) => {
-    emptySlots = req.body.slots;
+    buildings[1].slots = req.body.slots;
     res.send('Data received');
 });
 
-app.get('/slots', (req, res) => {
-    res.json({ id: id, slots: emptySlots });
+// Endpoints to get the number of empty slots for each building
+app.get('/slots/:buildingId', (req, res) => {
+    const buildingId = parseInt(req.params.buildingId, 10);
+    const building = buildings[buildingId];
+    if (building) {
+        res.json(building);
+    } else {
+        res.status(404).send('Building not found');
+    }
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
